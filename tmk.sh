29c40bc7c27998f14767d70f6fdf2e3f0c14132f
Andrew Reece <azmreece@gmail.com>
2015-05-24 17:19:53 +0100

    new file, add tmk additions

---
+++ tmk.sh
@@ -0,0 +1,92 @@
+# Vars
+# helpText =
+tmk1 = "
+##############
+# Tarmak  1  #
+##############
+
+# Top Row:
+keycode 18 = j
+# Middle Row: 
+keycode 36 = n
+keycode 37 = e
+# Bottom Row:
+keycode 49 = k
+
+##############"
+
+tmk2 = "
+##############
+# Tarmak  2  #
+##############
+
+# Top Row:
+keycode 18 = f
+keycode 20 = g
+# Middle Row:
+keycode 33 = t
+keycode 34 = j
+keycode 36 = n
+keycode 37 = e
+# Bottom Row:
+keycode 49 = k
+
+##############"
+tmk3 = "
+##############
+# Tarmak 3   #
+##############
+
+# Top Row:
+keycode 18 = f
+keycode 19 = j
+keycode 20 = g
+# Middle Row:
+keycode 31 = r
+keycode 32 = s
+keycode 33 = t
+keycode 34 = d
+keycode 36 = n
+keycode 37 = e
+# Bottom Row:
+keycode 49 = k
+
+##############"
+tmk4 = "
+######################
+# Tarmak 4           #
+######################
+
+# Top Row:
+keycode 18 = f
+keycode 19 = p
+keycode 20 = g
+keycode 21 = j
+keycode 24 = y
+keycode 25 = semicolon
+# Middle Row:
+keycode 31 = r
+keycode 32 = s
+keycode 33 = t
+keycode 34 = d
+keycode 36 = n
+keycode 37 = e
+keycode 39 = o
+# Bottom Row:
+keycode 49 = k
+
+######################"
+
+
+# take input from user
+## if ___ make keymaps in /usr/share/kbd/keymaps/
+
+###if n, enter file to use as template:
+
+## tell user how to apply kmp
+
+## if ___ show current kmp 
+
+## if __$ show tarmak$ kmp
+
+## if -h show help
